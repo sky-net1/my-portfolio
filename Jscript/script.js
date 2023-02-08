9dc571436803b981efbf97db91773bf071ea2fb5
sky-net1 <101561240+sky-net1@users.noreply.github.com>
2023-02-08 19:59:00 +0100

    Add files via upload

--- Jscript/script.js
+++ Jscript/script.js
@@ -1,6 +1,6 @@
 // Typing Animation
 var typed = new Typed(".typing", {
-    strings:["","UI/UX Designer","Web Developer","Graphic Designer","IT Support(Expert)"],
+    strings:["","UI/UX Designer","Web/Mobile-App Developer","Graphic Designer","IT Support Assistant"],
     typeSpeed:100,
     backSpeed:60,
     loop:true
